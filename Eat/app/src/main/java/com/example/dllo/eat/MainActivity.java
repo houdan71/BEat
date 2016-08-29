1bbf699bdb1bb3e1183fef69676cdf7753f96290
dllo <2272892654@qq.com>
2016-08-29 16:57:35 +0800

    创建吃工程

---
+++ Eat/app/src/main/java/com/example/dllo/eat/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dllo.eat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //组圆吃啥
+    }
+}
